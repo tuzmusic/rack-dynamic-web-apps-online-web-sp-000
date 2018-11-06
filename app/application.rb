63eb8f0a4ea7a1b7950a64c5620755d46aa04502
Jonathan Tuzman <tuzmusic@gmail.com>
2018-11-06 21:16:17 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -10,6 +10,12 @@ class Application
     num_1 = Kernel.rand(1..20)
     num_2 = Kernel.rand(1..20)
     num_3 = Kernel.rand(1..20)
+
+    if [num_1, num_2, num_3].uniq.count == 1
+      puts "You Win"
+    else
+      puts "You Lose"
+    end
   end
 
 end
